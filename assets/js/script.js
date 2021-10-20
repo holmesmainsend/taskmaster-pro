ad11d0e708928f4563cfc29cd518d48a75c29db1
Mona Lisa <holmesmainsend@gmail.com>
2021-10-20 15:36:46 -0400

    tested out moment.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,25 @@
 var tasks = {};
 
+var auditTask = function(taskEl) {
+ // get date from task element
+ var date = $(taskEl).find("span").text().trim();
+ // ensure it worked
+ console.log(date); 
+
+ // convert to moment object at 5:00pm
+ var time = moment(date, "L").set("hour", 17);
+ // remove any old classes from element
+ $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+ // apply new class if task is near/over due date
+ if (moment().isAfter(time)) {
+   $(taskEl).addClass("list-group-item-danger");
+ }
+ else if (Math.abs(moment().diff(time, "days")) <= 2) {
+  $(taskEl).addClass("list-group-item-warning");
+}
+};
+
 var createTask = function(taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
@@ -12,6 +32,9 @@ var createTask = function(taskText, taskDate, taskList) {
 
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
+
+   // check due date
+   auditTask(taskLi);
 
 
   // append to ul list on the page
@@ -87,26 +110,29 @@ $(this).replaceWith(taskP);
 
 // due date was clicked
 $(".list-group").on("click", "span", function() {
-  // get current text
-  var date = $(this)
-    .text()
-    .trim();
+// get current text
+var date = $(this).text().trim();
 
-  // create new input element
-  var dateInput = $("<input>")
-    .attr("type", "text")
-    .addClass("form-control")
-    .val(date);
+// create new input element
+var dateInput = $("<input>").attr("type", "text").addClass("form-control").val(date);
 
-  // swap out elements
-  $(this).replaceWith(dateInput);
+$(this).replaceWith(dateInput);
 
-  // automatically focus on new element
-  dateInput.trigger("focus");
+// enable jquery ui datepicker
+dateInput.datepicker({
+  minDate: 1,
+  onClose: function() {
+    // when calendar is closed, force a "change" event on the `dateInput`
+    $(this).trigger("change");
+  }
+});
+
+// automatically bring up the calendar
+dateInput.trigger("focus");
 });
 
 // value of due date was changed
-$(".list-group").on("blur", "input[type='text']", function() {
+$(".list-group").on("change", "input[type='text']", function() {
   // get current text
   var date = $(this)
     .val()
@@ -134,6 +160,9 @@ $(".list-group").on("blur", "input[type='text']", function() {
 
   // replace input with span element
   $(this).replaceWith(taskSpan);
+
+  // Pass task's <li> element into auditTask() to check new due date
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 
@@ -255,3 +284,8 @@ $("#trash").droppable({
     console.log("out");
   }
 });
+
+$("#modalDueDate").datepicker({
+  minDate: 1
+});
+
